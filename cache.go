f677a3dae2625309095d13c6e5ec23c0d03b552a
Arun Gautham Soundarrajan <arungautham60@gmail.com>
2025-11-01 23:22:01 +0000

    added comments for LRU cache

--- cache.go
+++ cache.go
@@ -17,6 +17,10 @@ type cacheResponse struct {
 	cachedAt   time.Time
 }
 
+// LRU cache stores the map of cache [key]:[cacheResponse](pointer to list.Element)
+// We use list.List which is go's implementation of Linkedlist to keep track of
+// Most used and lead used item.
+// Capacity is used to set the maximum capacity of the cache
 type LRUcache struct {
 	cache      map[string]*list.Element
 	linkedlist *list.List
@@ -24,25 +28,33 @@ type LRUcache struct {
 	mu         sync.Mutex
 }
 
+// Get functions gets the k which is the `GET:http://example.com/`
+// If the key exists, will return the cached response
 func (c *LRUcache) get(k string) (*cacheResponse, bool) {
 
 	c.mu.Lock()
 	defer c.mu.Unlock()
 	if elem, ok := c.cache[k]; ok {
 
+		// if the key exists, move the element to the front
 		c.linkedlist.MoveToFront(elem)
 		return elem.Value.(*cacheResponse), true
 	}
 	return nil, false
 }
 
+// Add an item to the cache if it doesn't exist already
+// Get the key for the cache and the response to store
 func (c *LRUcache) put(k string, resp cacheResponse) {
 
 	c.mu.Lock()
 	defer c.mu.Unlock()
+	// Push the stored element to most recently used
 	elem := c.linkedlist.PushFront(&resp)
 	c.cache[k] = elem
 
+	// Remove the tail if more than capacity after adding
+	// and delete the entry from cache map too
 	if c.linkedlist.Len() > c.capacity {
 		tail := c.linkedlist.Back()
 		c.linkedlist.Remove(tail)
